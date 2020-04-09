d42577ddaacb4942d28fd3a2915e7e049c68c72c
Jarad Aker <aker.jarad@gmail.com>
2020-04-09 12:37:29 -0400

    test test_parse_nick in parse.py completed by emily/jarad

--- tests/lykosTest.py
+++ tests/lykosTest.py
@@ -6,3 +6,9 @@ def test_start_cmd():
     print(check[0].decode())
     assert check[0].decode() == "Kevin!bncworld@I-Have.a.cool.vhost.com" and check[1] == "privmsg" \
            and check[2][0].decode() == "#mIRC" and check[2][1].decode() == 'I feel lucky today'
+
+def test_parse_nick():
+    user = "mywolfbot!~mywolfbot@pool-173-48-152-9.bstnma.fios.verizon.net"
+    userName = parse.parse_nick(user)
+    assert userName[0] == "mywolfbot" and userName[1] == None and userName[2] == "~mywolfbot" \
+           and userName[3] == "pool-173-48-152-9.bstnma.fios.verizon.net"
