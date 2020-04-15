574b5c90dd56adc07cb27411f04cda83bd133189
Jarad Aker <aker.jarad@gmail.com>
2020-04-15 17:48:00 -0400

    tests working:      -test_wolves_diseased: user given disease status and checks for it to be applied      -test_try_misdirection: user and distraction user made, user assigned misdirection and tries to misdirect returning the target

--- tests/lykosTest.py
+++ tests/lykosTest.py
@@ -3,33 +3,33 @@ import src
 import src.settings as var
 
 def test_parse_raw_irc_command():
-    element = bytes(":Kevin!bncworld@I-Have.a.cool.vhost.com PRIVMSG #mIRC :I feel lucky today", encoding="utf8")
-    check = parse.parse_raw_irc_command(element)
-    print(check[0].decode())
+    element = bytes( ":Kevin!bncworld@I-Have.a.cool.vhost.com PRIVMSG #mIRC :I feel lucky today", encoding="utf8" )
+    check = parse.parse_raw_irc_command( element )
+    print( check[0].decode() )
     assert check[0].decode() == "Kevin!bncworld@I-Have.a.cool.vhost.com" and check[1] == "privmsg" \
            and check[2][0].decode() == "#mIRC" and check[2][1].decode() == 'I feel lucky today'
 
 
 def test_parse_nick():
     user = "mywolfbot!~mywolfbot@pool-173-48-152-9.bstnma.fios.verizon.net"
-    userName = parse.parse_nick(user)
+    userName = parse.parse_nick( user )
     assert userName[0] == "mywolfbot" and userName[1] == None and userName[2] == "~mywolfbot" \
            and userName[3] == "pool-173-48-152-9.bstnma.fios.verizon.net"
 
 
 def test_add_users():
-    new_user = src.users.add(cli="6697", nick="Hoobshanker!None@None:None")
+    new_user = src.users.add( cli="6697", nick="Hoobshanker!None@None:None" )
     assert new_user.nick == "Hoobshanker"
 
 
 def test_get_users():
-    src.users.add(cli="6697", nick="Hoobshanker!None@None:None")
-    check = src.users.get(nick="Hoobshanker!None@None:None")
+    src.users.add( cli="6697", nick="Hoobshanker!None@None:None" )
+    check = src.users.get( nick="Hoobshanker!None@None:None" )
     assert check.nick == "Hoobshanker"
 
 
 def test_get_players():
-    new_user = src.users.add(cli="6697", nick="Hoobshanker!None@None:None")
+    new_user = src.users.add( cli="6697", nick="Hoobshanker!None@None:None" )
     var.MAIN_ROLES = {"Hoobshanker": "villager"}
     var.ALL_PLAYERS = [new_user.nick]
     check = src.functions.get_players()
@@ -37,22 +37,36 @@ def test_get_players():
 
 
 def test_is_silent():
-    new = src.users.add(cli="6697", nick="Rissa09!None@None:None")
-    src.status.add_silent(var=None, user=new)
-    assert src.status.is_silent(var=None, user=new)
+    new = src.users.add( cli="6697", nick="Rissa09!None@None:None" )
+    src.status.add_silent( var=None, user=new )
+    assert src.status.is_silent( var=None, user=new )
 
 
 def test_add_dying():
     var.GAME_ID = 1
-    new = src.users.add(cli="6697", nick="Rissa09!None@None:None")
+    new = src.users.add( cli="6697", nick="Rissa09!None@None:None" )
     var.MAIN_ROLES = {new: "villager"}
     var.ALL_PLAYERS = [new]
-    assert src.status.add_dying(var=var, player=new, killer_role="wolf", reason="kill")
+    assert src.status.add_dying( var=var, player=new, killer_role="wolf", reason="kill" )
 
 
 def test_is_dying():
-    new = src.users.add(cli="6697", nick="Rissa09!None@None:None")
+    new = src.users.add( cli="6697", nick="Rissa09!None@None:None" )
     var.MAIN_ROLES = {new: "villager"}
     var.ALL_PLAYERS = [new]
-    src.status.add_dying(var=var, player=new, killer_role="wolf", reason="kill")
-    assert src.status.is_dying(var, new)
+    src.status.add_dying( var=var, player=new, killer_role="wolf", reason="kill" )
+    assert src.status.is_dying( var, new )
+
+
+def test_wolves_diseased():
+    user = src.users.add( cli="6697", nick="chubbychicken123!None@None:None" )
+    src.status.add_disease( var=None, target=user )
+    src.status.wolves_diseased(var = None)
+    assert True
+
+def test_try_misdirection():
+    user = src.users.add( cli="6697", nick="chubbychicken123!None@None:None" )
+    dist = src.users.add( cli="6697", nick="tubbyturkey123!None@None:None" )
+    src.status.add_misdirection( var = None, user= user, as_actor=False, as_target=False )
+    assert src.status.try_misdirection(var = None, actor= dist, target= user)
+
