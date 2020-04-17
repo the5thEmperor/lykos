38ff09bdc295a38a5413e825481bc93c5b82f8c1
Marissa <mboucher@student.bridgew.edu>
2020-04-17 10:21:15 -0400

    Commented out if statement in warlock.py that disables traitors being cursed. Added is_cursed function to warlock.py Added if statement in the myrole command of wolfgame.py calling the is_cursed function and implementing correct role. Added empty is_CURSED list to warlock.py and appended target to it. Added “cursed traitor” character to messages/en.json. Added test_is_cursed function.

--- tests/lykosTest.py
+++ tests/lykosTest.py
@@ -1,6 +1,7 @@
 import oyoyo.parse as parse
 import src
 import src.settings as var
+from src.roles import warlock
 
 def test_parse_raw_irc_command():
     element = bytes(":Kevin!bncworld@I-Have.a.cool.vhost.com PRIVMSG #mIRC :I feel lucky today", encoding="utf8")
@@ -56,3 +57,11 @@ def test_is_dying():
     var.ALL_PLAYERS = [new]
     src.status.add_dying(var=var, player=new, killer_role="wolf", reason="kill")
     assert src.status.is_dying(var, new)
+
+
+def test_is_cursed():
+    new = src.users.add(cli="6697", nick="Rissa09!None@None:None")
+    var.MAIN_ROLES = {new: "cursed traitor"}
+    var.ALL_PLAYERS = [new]
+    warlock.is_CURSED.append(new)
+    assert warlock.is_cursed(new)
